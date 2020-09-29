e8644d7f992c8bc047160fee8c7894f25ee38e7a
Ali Kaname <72041734+Alikaname@users.noreply.github.com>
2020-09-29 18:15:36 +0300

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -2,16 +2,15 @@ import smtplib
 from email.message import EmailMessage
 
 email = EmailMessage()
-email["from"] = 'Ali'
-email["to"] = 'mircea.mitroi10@gmail.com'
-email['subject'] = "this email was sent automatically"
+email["from"] = 'your name'
+email["to"] = 'email'
+email['subject'] = "subject"
 
-email.set_content(
-    "This email is sent from python \n PS we're still up for gta later")
+email.set_content("your message")
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('ali.kanameh@gmail.com', "0214300322")
+    smtp.login('your', "your pass")
     smtp.send_message(email)
     print("all good boss!")
